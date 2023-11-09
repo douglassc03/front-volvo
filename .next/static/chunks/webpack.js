bf209218c52de251619c8e2de6b2d7b54eb56942
Douglas <therayjuss@gmail.com>
2023-11-09 13:55:51 -0300

    feat: card cards design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8484696656e6fd88"; }
+/******/ 		__webpack_require__.h = function() { return "01b31f4b17ec3164"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,4 +1,4 @@
-  import { CardContent, Card, Text, Spacer } from "vcc-ui"
+  import { CardContent, Card, Text, Spacer, Flex, Row, Link } from "vcc-ui"
   import { Car } from "../types/car.interface"
   
   
@@ -6,14 +6,33 @@
         car: Car
   }
 
-  export function CarCard(proops:Cardprops){
+  export function CarCard({car} : Cardprops){
     return(
-        <Card href={"/learn" + proops.car.id}>
+        <Card href={"/learn" + car?.id}>
+
          <CardContent>
-            <Text subStyle="emphasis">{proops.car.bodyType}</Text>
-            <Text variant="ootah">{proops.car.modelName}</Text>
+            <Text variant="bates" subStyle="emphasis">{car?.bodyType}</Text>
+            <Flex>
+              <Row>
+                <Text variant="amundsen">{car?.modelName}</Text>
+                <Text variant="bates" subStyle="inline-link">{car?.modelType}</Text>
+              </Row>
+            </Flex>
             <Spacer /> 
-            <Text>This is a is a link that will take you to volvocars.com</Text>
+            <img src={car.imageUrl} alt={car.modelName} width={200} height={150}/>
+            <Flex  extend={{
+                    justifyContent: 'center',
+                    alignItems: 'flexstart',
+                    flexDirection: 'row',
+                    maxWidth: 200
+                }}>
+                <Link href="https://www.volvocars.com/" arrow="right">
+                  SHOP
+                </Link>
+                <Link href="https://www.volvocars.com/" arrow="right">
+                  LEARN
+                </Link>
+            </Flex>
         </CardContent>
       </Card>
     ) 
